6abdfc44d01966588392f418cda46802feeb5db3
Bekzat Tolegenov <tolegenovbt@gmail.com>
2020-03-19 11:49:16 +0600

    week8

---
+++ week8/for/H.py
@@ -0,0 +1,9 @@
+a = int(input())
+
+arr = ''
+
+for i in range(1,a + 1):
+    if(a % i == 0):
+        arr += f'{i} '
+
+print(arr)
---
+++ week8/myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\аскар\desktop\webdevelopment\wd2020\week8\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
